19d2f7e61e558f722c8ab886ef4848141eef692e
Peeter Roop <petsens@users.noreply.github.com>
2017-09-14 11:33:42 +0300

    Lisatud on muutujad ja IF command

--- esimene.php
+++ esimene.php
@@ -1,3 +1,24 @@
+<?php
+	//muutujad
+	$myName = "Peeter";
+	$myFamilyName = "Roop";
+	
+	//hindan äeva osa (võrdlemine < > <= >= != == )
+	$hourNow = date("H");
+	$partOfDay = "";
+	if ($hourNow < 8){
+		$partOfDay = "Varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow < 16){
+		$partOfDay = "Koolipäev";
+	}
+	if ($hourNow > 16){
+		$partOfDay = "Vaba aeg";
+	}
+	//echo $partOfDay;
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,12 +27,15 @@
 
 </head>
 <body>
-	<h1>Peeter Roop, veebiprogrammeerimine</h1>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine</h1>
 	<p>See veebileht on õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
 	<p>Eesti ilmad on praegu väga halvad.</p>
 	<p>I am mr. Robot<p>
 	<?php
-		echo "Algas php õppimine.";
+		echo "<p>Algas php õppimine.</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
+		echo ", hetkel on " .$partOfDay .".</p>";
 	?>
 </body>
 </html>
