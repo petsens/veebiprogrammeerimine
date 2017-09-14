de5624c4c45d55e18919a4d71ae46b0e08001fe7
Peeter Roop <petsens@users.noreply.github.com>
2017-09-14 10:47:10 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Peeter Roop programmeerib veebi</title>
+
+</head>
+<body>
+	<h1>Peeter Roop, veebiprogrammeerimine</h1>
+	<p>See veebileht on õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Eesti ilmad on praegu väga halvad.</p>
+	<p>I am mr. Robot<p>
+	<?php
+		echo "Algas php õppimine.";
+	?>
+</body>
+</html>
